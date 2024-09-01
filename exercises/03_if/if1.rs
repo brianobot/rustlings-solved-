ae80a003ced990a638702f5a12e7e242255a8957
Brian Obot <brianobot9@gmail.com>
2024-09-01 11:07:14 +0100

    initial commit 🍃

---
+++ exercises/03_if/if1.rs
@@ -0,0 +1,44 @@
+fn bigger(a: i32, b: i32) -> i32 {
+    // TODO: Complete this function to return the bigger number!
+    // If both numbers are equal, any of them can be returned.
+    // Do not use:
+    // - another function call
+    // - additional variables
+    if a > b {
+        a
+    } else if a < b {
+        b
+    } else {
+        a
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+    let ten = bigger(10, 5);
+    let two = bigger(2, -2);
+
+    println!("ten: {ten}");
+    println!("two: {two}");
+}
+
+// Don't mind this for now :)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
+    }
+
+    #[test]
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
+    }
+
+    #[test]
+    fn equal_numbers() {
+        assert_eq!(42, bigger(42, 42));
+    }
+}
